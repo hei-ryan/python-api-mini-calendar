c1ed70ff0e03a49f82ce99d94b76f5c75b44bbff
Ryan Andriamahery <ryan@hei.school>
2025-07-14 10:57:02 +0300

    feat GET / returning welcome.html file

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from starlette.responses import Response
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    with open("welcome.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return Response(content=html_content, status_code=200, media_type="text/html")
