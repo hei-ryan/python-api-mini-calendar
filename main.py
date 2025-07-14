2d1b37ce21382a6c48fa974a4b0ac9eec1339450
Ryan Andriamahery <ryan@hei.school>
2025-07-14 11:29:12 +0300

    feat: handle as JSON type unknown path with 404 response

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import json
 from fastapi import FastAPI
 from starlette.responses import Response
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     with open("welcome.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return Response(content=html_content, status_code=200, media_type="text/html")
+
+
+@app.get("/{full_path:path}")
+def catch_all(full_path: str):
+    not_found_message = {"detail": f"Page '/{full_path}' not found"}
+    return Response(content=json.dumps(not_found_message), status_code=404, media_type="application/json")
